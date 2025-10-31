35e19f9b3b38ee9cb98ed7a70704cf56028cea75
cristinalopez-uritec <cristina.lopez@uritec.es>
2025-10-31 09:31:30 +0100

    same as previous commit

--- testDLLrecordsNatacion/Log.cs
+++ testDLLrecordsNatacion/Log.cs
@@ -11,18 +11,18 @@ namespace testDLLrecordsNatacion
         //Logger constructor
         static Log()
         {
-            //LogManager.ReconfigExistingLoggers();
+            LogManager.ReconfigExistingLoggers();
             Instance = LogManager.GetCurrentClassLogger();
 
-
             LogManager.Setup().LoadConfiguration(builder => {
-                builder.ForLogger().FilterMinLevel(LogLevel.Info).WriteToFile(fileName: "Log_debugInfo.txt");
-                builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToFile(fileName: "Log_debugInfo.txt");
-                builder.ForLogger().FilterMinLevel(LogLevel.Warn).WriteToFile(fileName: "Log_warnErrorFatal.txt");
-                builder.ForLogger().FilterMinLevel(LogLevel.Error).WriteToFile(fileName: "Log_warnErrorFatal.txt");
-                builder.ForLogger().FilterMinLevel(LogLevel.Fatal).WriteToFile(fileName: "Log_warnErrorFatal.txt");
+                builder.ForLogger().FilterMinLevel(LogLevel.Info).WriteToFile(fileName: "Logs/Log_debugInfo.txt");
+                builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToFile(fileName: "Logs/Log_debugInfo.txt");
+                builder.ForLogger().FilterMinLevel(LogLevel.Warn).WriteToFile(fileName: "Logs/Log_warnErrorFatal.txt");
+                builder.ForLogger().FilterMinLevel(LogLevel.Error).WriteToFile(fileName: "Logs/Log_warnErrorFatal.txt");
+                builder.ForLogger().FilterMinLevel(LogLevel.Fatal).WriteToFile(fileName: "Logs/Log_warnErrorFatal.txt");
             });
 
+            LogManager.Configuration.Reload();
         }
 
     }
